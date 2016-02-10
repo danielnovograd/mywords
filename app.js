60b9c71f7708269d53a22f67121b6c7ff5ba2960
Dan Novograd <dnovograd@gmail.com>
2016-02-09 20:18:49 -0800

    resolving promises

--- app.js
+++ app.js
@@ -21,24 +21,13 @@ app.controller('MainCtrl',
         $scope.showLookups = false;
         $scope.test = 'TESTING TESTING';
         $scope.queryWord = function() {
-            if (!$scope.targetWord || $scope.targetWord === '') {
-                return;
-            }
-            if ($scope.lookupHistory.indexOf($scope.targetWord) < 0) {
-                //lookup word def
-                //lookup word etym
-                $q.all({
-                    definitionArray: $http.get('http://api.wordnik.com:80/v4/word.json/' + $scope.targetWord + '/definitions?limit=5&includeRelated=true&sourceDictionaries=wiktionary&useCanonical=true&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'),
-                    etymology: $http.get('http://api.wordnik.com:80/v4/word.json/' + $scope.targetWord + '/etymologies?useCanonical=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5')
-                }).then(function(results) {
-                    $scope.wordDefinition = results.definitionArray.data[0].text;
-                    $scope.wordEtymology = results.etymology.data[0];
-                    $scope.lookupHistory.push($scope.targetWord);
-                    $scope.lookupList = $scope.lookupHistory.join(", ");
-                    $scope.showLookups = true;
-                })
-
-            }
+          $http({
+            method: 'POST',
+            url: '/api/word',
+            data: JSON.stringify({data: $scope.targetWord})
+          }).then(function(response){
+            console.log(response);
+          })
         };
         $scope.targetWord = '';
         $scope.lookupHistory = words.lookupHistory;
