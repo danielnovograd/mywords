35e8496572a488d7953bef2ef1d8b0146ceb8f61
Dan Novograd <dnovograd@gmail.com>
2016-05-20 11:39:30 -0400

    (feat) Create persistent word list

--- client/app.js
+++ client/app.js
@@ -18,6 +18,8 @@ app.config([
 
 app.controller('MainCtrl',
     function($scope, $http, words) {
+        $scope.currentList = words.currentList;
+
         $scope.showLookups = false;
         $scope.lookupList = words.lookupHistory;
 
@@ -28,7 +30,7 @@ app.controller('MainCtrl',
             $scope.wordDefinition = response.data[0];
             $scope.wordEtymology = response.data[1];
             if ($scope.wordDefinition.length > 0 || $scope.wordEtymology.length > 0) {
-                $scope.lookupList.push($scope.targetWord);    
+                $scope.lookupList.push($scope.targetWord);
             }
             $scope.showLookups = true;
           })
@@ -44,10 +46,34 @@ app.controller('MainCtrl',
         })
         }
         $scope.targetWord = '';
-
+        $scope.save = function(word) {
+            words.saveToList(word);
+        };
+        $scope.clearList = function() {
+            if(confirm("Are you sure you want to delete?")) {
+                words.clearList();
+            };
+        }
     });
 
 app.factory('words', function($http) {
+    var currentList = {};
+
+    var loadList = function() {
+        var savedList = localStorage.getItem('wordList');
+        if (savedList === null) {
+            localStorage.setItem('wordList', JSON.stringify(currentList));
+        }
+        else {
+            currentList = JSON.parse(savedList);
+        }
+    };
+
+    var clearList = function() {
+        currentList = {};
+        localStorage.setItem('wordList', JSON.stringify(currentList));
+    };
+
     var queryWord = function(word) {
         return $http({
             method: 'POST',
@@ -55,9 +81,24 @@ app.factory('words', function($http) {
             data: JSON.stringify({data: word})
         });
     };
+
+    var saveToList = function(word) {
+        currentList[word] = true;
+        localStorage.setItem('wordList', JSON.stringify(currentList));
+        loadList();
+        console.log("WORD LIST", JSON.parse(localStorage.getItem('wordList')));
+    };
+
+    var localList = loadList();
     var lookupHistory = [];
+
     return {
+        currentList: currentList,
+        loadList: loadList,
+        saveToList: saveToList,
         queryWord: queryWord,
-        lookupHistory: lookupHistory
+        localList: localList,
+        lookupHistory: lookupHistory,
+        clearList: clearList
     };
 });
