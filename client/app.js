29e6c2252403a459ce8a1eaa6e80dae136f0ae4f
Dan Novograd <dnovograd@gmail.com>
2016-07-08 12:00:54 -0400

    Implement watch task

--- client/app.js
+++ client/app.js
@@ -4,14 +4,12 @@ app.config([
     '$stateProvider',
     '$urlRouterProvider',
     function($stateProvider, $urlRouterProvider) {
-
         $stateProvider
             .state('home', {
                 url: '/home',
                 templateUrl: '/home.html',
                 controller: 'MainCtrl'
             });
-
         $urlRouterProvider.otherwise('home');
     }
 ]);
