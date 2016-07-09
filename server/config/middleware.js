6b731862bf2238b1c2b7e13438a13d2a48a04405
Dan Novograd <dnovograd@gmail.com>
2016-07-09 15:34:19 -0400

    (feat) Set up wordList middleware

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,13 +1,16 @@
 var bodyParser = require('body-parser');
 
-module.exports = function (app, express) {
+module.exports = function(app, express) {
+  var lookupRouter = express.Router();
+  var wordListRouter = express.Router();
+
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../../client'));
 
-  var lookupRouter = express.Router();
   app.use('/api/lookups', lookupRouter);
-  require('../lookups/lookupRoutes.js')(lookupRouter);
-  
-};
+  app.use('/api/save', wordListRouter);
 
+  require('../wordLists/wordListRoutes.js')(wordListRouter)
+  require('../lookups/lookupRoutes.js')(lookupRouter);
+};
