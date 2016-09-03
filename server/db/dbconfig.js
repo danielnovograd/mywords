583dcd46a54b3e8c2cb86b67f3891bdb471a0f94
Dan Novograd <dnovograd@gmail.com>
2016-09-03 07:30:41 -0400

    (fix) Update db config

--- server/db/dbconfig.js
+++ server/db/dbconfig.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var DB_URI = process.env.MONGOLAB_URI || 'mongodb://localhost/wordrly';
+
 mongoose.connect(DB_URI, function(err) {
   if (err) {
     console.log('connection err: ', err);
